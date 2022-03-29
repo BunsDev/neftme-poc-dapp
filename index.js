7fcb409139dacbbd1990fd7d52ad506975314b93
Luís Neves <luismanuelneves5@gmail.com>
2022-03-29 16:35:15 +0100

    Revert "Valora Integration"
    This reverts commit 29baf77cf24ca26f24981d8e0d07d12d61187f66.

--- index.js
+++
@@ -1,2 +0,0 @@
-require('web3-providers-http/lib/index.js')
-require('../node_modules/http/index.js')
--- metro.config.js
+++ metro.config.js
@@ -2,31 +2,17 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
-  
+
   const { transformer, resolver } = config;
 
   config.transformer = {
     ...transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   };
-
   config.resolver = {
-    extraNodeModules: {
-    crypto: require.resolve('crypto-browserify'),
-    url: require.resolve('url/'),
-    fs: require.resolve('expo-file-system'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify/browser.js'),
-    stream: require.resolve('readable-stream'),
-    vm: require.resolve('vm-browserify'),
-    net: require.resolve('react-native-tcp'),
-    path: require.resolve('path-browserify'),
-    ...config.resolver?.resolver?.extraNodeModules,
-    },
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg', 'js', 'jsx', 'ts', 'tsx', 'cjs'],
+    sourceExts: [...resolver.sourceExts, 'svg'],
   };
 
   return config;
