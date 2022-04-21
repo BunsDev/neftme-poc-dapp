1c91c775327a970d8f0f3cb7b4747b5ec15ec6c0
Gonçalo Marques <goncalo.marques@talkdesk.com>
2022-04-21 12:40:14 +0100

    Add chain verification

--- src/components/start/wallet.jsx
+++ src/components/start/wallet.jsx
@@ -1,74 +1,67 @@
-import React, { useCallback, useEffect } from "react";
-import { Image, View, Text, Alert } from "react-native";
-import { Button } from "@library";
-import { useWalletConnect } from "@walletconnect/react-native-dapp";
-import { useNavigation } from "@react-navigation/native";
-import { isNewUser } from "@services/user";
-import Constants from "expo-constants";
-import { useChainCheck } from "@hooks";
-import { withOnboardingView } from "@hocs";
-import styles from "./wallet_styles";
-import Web3 from "web3";
-import WalletConnectProvider from "@walletconnect/web3-provider";
+import React, { useCallback, useEffect } from 'react';
+import {
+  Image, View, Text, Alert,
+} from 'react-native';
+import { Button } from '@library';
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import { useNavigation } from '@react-navigation/native';
+import { isNewUser } from '@services/user';
+import Constants from 'expo-constants';
+import { useChainCheck } from '@hooks';
+import { withOnboardingView } from '@hocs';
+import styles from './wallet_styles';
 
-const walletConnectImage = require("@assets/wallet_connect.png");
-const metamaskImage = require("@assets/metamask.png");
-const valoraImage = require("@assets/valora.png");
-const rainbowImage = require("@assets/rainbow.png");
+const walletConnectImage = require('@assets/wallet_connect.png');
+const metamaskImage = require('@assets/metamask.png');
+const valoraImage = require('@assets/valora.png');
+const rainbowImage = require('@assets/rainbow.png');
 
-const IconComponent = (source) => () =>
-  <Image source={source} style={styles.image} />;
+const IconComponent = (source) => () => <Image source={source} style={styles.image} />;
 
 const goToNextStep = async (navigation) => {
   if (await isNewUser()) {
-    navigation.navigate("Start", {
-      screen: "Categories",
+    navigation.navigate('Start', {
+      screen: 'Categories',
     });
   } else {
-    navigation.navigate("Home");
+    navigation.navigate('Home');
   }
 };
 
 const Wallet = () => {
   const connector = useWalletConnect();
   const navigation = useNavigation();
-  const {changeToAlfajores, addNEFTtoWallet } = useChainCheck();
+  const { changeToAlfajores, currentChainId, addNEFTtoWallet } = useChainCheck();
 
   const connectWallet = useCallback(() => {
-    if (
-      !connector?.connected ||
-      connector?.chainId !== Constants.manifest.extra.chainId
-    ) {
-      Alert.alert(
-        "Connected to wrong blockchain!",
-        "You are currently not connected to the Alfajores Testnet, to procede, please switch network",
-        [{ text: "Switch", onPress: () => changeToAlfajores() }]
-      );
-    } else {
-      try {
-        connector.connect().catch(() => {});
-      } catch (err) {
-        console.log(err);
-        // TODO: LOG ERRORS;
-      }
+    try {
+      connector.connect().catch(() => { });
+    } catch (err) {
+      // TODO: LOG ERRORS;
     }
   }, [connector]);
 
   useEffect(async () => {
-    if (
-      connector?.connected &&
-      connector?.chainId === Constants.manifest.extra.chainId
-    ) {
-      if (await isNewUser()) {
-        addNEFTtoWallet().then(
-        navigation.navigate("Start", {
-          screen: "Categories",
-        }));
-      } else {
-        navigation.navigate("Home");
+    if (connector) {
+      if (connector.connected && currentChainId === Constants.manifest.extra.chainId) {
+        if (await isNewUser()) {
+          addNEFTtoWallet().then(
+            navigation.navigate('Start', {
+              screen: 'Categories',
+            }),
+          );
+        } else {
+          navigation.navigate('Home');
+        }
+      } else if (connector.connected && currentChainId !== Constants.manifest.extra.chainId) {
+        Alert.alert(
+          'Wrong blockchain',
+          'You are not connected to the Alfajores Testnet. To proceed, please switch network',
+          [{ text: 'Switch', onPress: changeToAlfajores }],
+        );
       }
     }
-  }, [connector]);
+  }, [connector, currentChainId]);
 
   return (
     <View style={styles.container}>
@@ -83,21 +76,21 @@ const Wallet = () => {
         text="MetaMask"
         textStyle={styles.walletButtonText}
         buttonStyle={[styles.walletButton, styles.disabledButton]}
-        onPress={() => {}}
+        onPress={() => { }}
         Icon={IconComponent(metamaskImage)}
       />
       <Button
         text="Valora"
         textStyle={styles.walletButtonText}
         buttonStyle={[styles.walletButton, styles.disabledButton]}
-        onPress={() => {}}
+        onPress={() => { }}
         Icon={IconComponent(valoraImage)}
       />
       <Button
         text="Rainbow"
         textStyle={styles.walletButtonText}
         buttonStyle={[styles.walletButton, styles.disabledButton]}
-        onPress={() => {}}
+        onPress={() => { }}
         Icon={IconComponent(rainbowImage)}
       />
       <Button
@@ -117,5 +110,5 @@ const Wallet = () => {
 };
 
 export default withOnboardingView((navigation) => goToNextStep(navigation))(
-  Wallet
+  Wallet,
 );
