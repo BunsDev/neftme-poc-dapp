5214137ff8ef764fb608fbaee76c69c02020c1a8
Luís Neves <luismanuelneves5@gmail.com>
2022-04-21 18:29:03 +0100

    Staking Fix and Dynamic Stake Amount added

--- src/components/home/timeline/nft/tokenomics.jsx
+++ src/components/home/timeline/nft/tokenomics.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
 import TokenIcon from '@assets/icons/token.svg';
 import styles from './styles';
+import { useSmartContract } from '../../../../hooks';
+import Constants from 'expo-constants';
 
-const Tokenomics = ({ nft }) => (
+const Tokenomics = ({ nft }) => {
+
+  const [stakedAmount, setStakedAmount] = useState(0);
+  const { getContractMethods } = useSmartContract();
+
+  const getNFTTotalStakedAmount = async () => {
+    const contractMethods = await getContractMethods(
+      Constants.manifest.extra.neftmeErc721Address,
+    );
+    try{
+    return contractMethods.nftTotalStaked(nft?.tokenId).call()
+      .then((a) => { setStakedAmount(a * 10 ** -18); });
+    }catch(err){
+      //log errors
+    }
+  };
+
+  useEffect(async () => {
+    getNFTTotalStakedAmount();
+  }, []);
+
+  return(
   <View style={styles.tokenomicsContainer}>
     <View style={styles.stakedContainer}>
       <TokenIcon width={34} height={34} />
       <View>
         <Text style={styles.stakedStyle}>staked</Text>
-        <Text style={styles.neftsAmountStyle}>{`${nft.staked} nefts`}</Text>
+        <Text style={styles.neftsAmountStyle}>{`${stakedAmount} nefts`}</Text>
       </View>
     </View>
     <View style={styles.verticalLine} />
@@ -21,8 +44,9 @@ const Tokenomics = ({ nft }) => (
       </Text>
       <Text style={[styles.economicDetails, styles.fontWeight700]}>{` ${nft.supporters} supporters`}</Text>
     </View>
-  </View>
-);
+  </View>);
+
+};
 
 Tokenomics.propTypes = {
   nft: PropTypes.shape({
