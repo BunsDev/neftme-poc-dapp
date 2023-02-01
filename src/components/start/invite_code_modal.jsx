c7336df14f478b5ad40df06a407c82860059e7f2
Luís Neves <luismanuelneves5@gmail.com>
2023-02-01 23:49:06 +0000

    Minor button fix

--- src/components/start/invite_code_modal.jsx
+++ src/components/start/invite_code_modal.jsx
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   button: {
-    width: '100%',
+    width: '90%',
   },
 });
 
@@ -99,7 +99,6 @@ const InviteCodeModal = () => {
               <Button
                 text="Submit"
                 onPress={() => console.log('a')}
-                textStyle={{}}
                 buttonStyle={styles.button}
               />
             </View>
