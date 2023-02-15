bc569404a837b0e0e27583748556a74446bb57e9
Luís Neves <luismanuelneves5@gmail.com>
2023-02-15 01:20:28 +0000

    Add Search User UI | Create Challenge Modal

--- src/components/challenges/select_user.tsx
+++ src/components/challenges/select_user.tsx
@@ -1,44 +1,104 @@
-import React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+} from 'react-native';
 import BackIcon from '@assets/icons/back.svg';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#2B2F3A',
+    backgroundColor: '#13131F',
   },
-  greeting: {
+  backIcon: {
+    marginTop: '13%',
+    marginLeft: '5%',
+  },
+  textStyle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16,
+    fontWeight: '500',
+    color: '#FFF',
   },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
+  textContainer: {
+    flexDirection: 'row',
+    marginLeft: '20%',
+    marginTop: '10%',
   },
-  modalView: {
-    width: '80%',
-    height: '70%',
-    backgroundColor: '#2B2F3A',
+  selected: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+  },
+  individualText: {
+    paddingHorizontal: 20,
+  },
+  searchContainer: {
+    flexDirection: 'column',
+    marginTop: '13%',
+  },
+  input: {
+    height: 40,
+    margin: 12,
     borderWidth: 1,
+    borderColor: '#2B2F3A',
+    backgroundColor: '#2B2F3A',
     borderRadius: 10,
-    borderColor: '#555555',
+    paddingHorizontal: 10,
+    color: '#FFF',
   },
-  buttonClose: {},
+  aroundYouContainer: {
+    alignSelf: 'center',
+  },
 });
+
+const options = [
+  {
+    text: 'Search',
+  },
+  {
+    text: 'Around You',
+  },
+];
 
 export type Props = {};
 
 const SelectUser: React.FC<Props> = () => {
+  const [selected, setSelected] = useState(0);
+  const [search, setSearch] = useState('');
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.centeredView}>
-        <BackIcon width={30} height={30} />
+      <TouchableOpacity style={styles.backIcon}>
+        <BackIcon width={25} height={25} />
       </TouchableOpacity>
+      <View style={styles.textContainer}>
+        {options.map((item: any, index) => (
+          <TouchableOpacity
+            onPress={() => setSelected(index)}
+            style={styles.individualText}
+          >
+            <View style={selected === index ? styles.selected : null}>
+              <Text style={[styles.textStyle]}>{item.text}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={styles.searchContainer}>
+        {selected === 0 ? (
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            onChange={(event) => setSearch(event.nativeEvent.text)}
+            value={search}
+          />
+        ) : (
+          <View style={styles.aroundYouContainer}>
+            <Text style={[styles.textStyle]}>Coming Soon!</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 };
