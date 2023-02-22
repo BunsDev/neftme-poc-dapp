7eb8e0f3a6793338524b5a5bad4ba2d6419ae152
Luís Neves <luismanuelneves5@gmail.com>
2023-02-22 18:37:00 +0000

    Update Home Screen

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,37 +1,23 @@
-import React, { useState } from 'react';
-import { Modal, Text, Image, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { ProfileImage } from '@library';
-import BellIcon from '@assets/icons/bell.svg';
 import ChallengeIcon from '@assets/icons/challenge_header_icon.svg';
-import { useGetCurrentUserQuery } from '@features/current_user';
+import DMIcon from '@assets/icons/direct_message.svg';
 import styles from './styles';
-import NotificationModal from './notification_modal';
 
 const logo = require('@assets/logo_home.webp');
 
 const Header = () => {
   const navigation = useNavigation();
-  const [modalVisible, setModalVisible] = useState(false);
-  const { data: currentUser } = useGetCurrentUserQuery();
 
   return (
     <View>
-      <View style={styles.centeredView}>
-        <NotificationModal isVisible={modalVisible} setIsVisible={setModalVisible}/>
-      </View>
       <View style={styles.header}>
         <View style={styles.subHeaderRight}>
           <Image source={logo} style={styles.logo} />
         </View>
         <View style={styles.subHeaderLeftContainer}>
           <View style={styles.subHeaderLeft}>
-            <TouchableOpacity onPress={() => setModalVisible(true)}>
-              <View>
-                <BellIcon style={styles.bellIcon} width={25} height={22} />
-                <View style={styles.notificationBadge} />
-              </View>
-            </TouchableOpacity>
             <TouchableOpacity
               onPress={() => navigation.navigate('StartChallenge')}
             >
@@ -39,21 +25,8 @@ const Header = () => {
                 <ChallengeIcon style={styles.bellIcon} width={35} height={30} />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('MyProfile')}>
-              {currentUser ? (
-                <ProfileImage
-                  profileImage={currentUser?.profileImage}
-                  containerStyle={{
-                    ...styles.profileImageContainer,
-                    backgroundColor: currentUser?.profileColor,
-                  }}
-                  imageStyle={styles.profilePhoto}
-                  avatarWidth={30}
-                  avatarHeight={30}
-                />
-              ) : (
-                <View style={styles.profilePhotoPlaceholder} />
-              )}
+            <TouchableOpacity>
+              <DMIcon width={30} height={30} />
             </TouchableOpacity>
           </View>
         </View>
--- src/components/library/bottom_bar.jsx
+++ src/components/library/bottom_bar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import HomeIcon from '@assets/icons/home.svg';
 import CreateIcon from '@assets/icons/create_nft.svg';
 import SearchIcon from '@assets/icons/search.svg';
+import BellIcon from '@assets/icons/bell.svg';
+import { ProfileImage } from '@library';
+import { useGetCurrentUserQuery } from '@features/current_user';
+import NotificationModal from '../home/notification_modal';
 
 const styles = StyleSheet.create({
   bottomBar: {
@@ -14,20 +18,65 @@ const styles = StyleSheet.create({
     bottom: 0,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   createNftIcon: {
     marginHorizontal: 75,
+  },
+  profileImageContainer: {
+    width: 25,
+    height: 25,
+  },
+  profilePhoto: {
+    width: 40,
+    height: 40,
   },
 });
 
 const BottomBar = () => {
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const { data: currentUser } = useGetCurrentUserQuery();
+
   return (
     <View style={styles.bottomBar}>
-      <HomeIcon width={32} height={33} onPress={() => navigation.navigate('Home')} />
-      <CreateIcon style={styles.createNftIcon} width={43.35} height={44} onPress={() => navigation.navigate('CreateNFT')} />
-      <SearchIcon width={32} height={33} onPress={() => navigation.navigate('Search')} />
+      <NotificationModal
+        isVisible={modalVisible}
+        setIsVisible={setModalVisible}
+      />
+
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <HomeIcon width={25} height={25} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+        <SearchIcon width={25} height={25} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('CreateNFT')}>
+        <CreateIcon width={45} height={45} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <BellIcon width={25} height={25} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('MyProfile')}>
+        {currentUser ? (
+          <ProfileImage
+            profileImage={currentUser?.profileImage}
+            containerStyle={{
+              ...styles.profileImageContainer,
+              backgroundColor: currentUser?.profileColor,
+            }}
+            imageStyle={styles.profilePhoto}
+            avatarWidth={25}
+            avatarHeight={25}
+          />
+        ) : (
+          <View style={styles.profilePhotoPlaceholder} />
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
