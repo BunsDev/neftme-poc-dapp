156f6b86de0fdc085bce4895545503b892a2efe3
Luís Neves <luismanuelneves5@gmail.com>
2023-02-27 13:45:08 +0000

    Add Challenge POST and Delete Requests

---
+++ src/services/challenge/challenge.ts
@@ -0,0 +1,47 @@
+import Constants from 'expo-constants';
+import { getData } from '../storage';
+import Challenge from '../../model/challenge_model';
+
+export default async (challenge: Challenge) => {
+  try {
+
+    const formData = new FormData();
+    formData.append('user_challenged', challenge.getUserChallenged());
+    formData.append('nft', challenge.getNft());
+    formData.append('description', challenge.getDescription());
+    formData.append('value', `${challenge.getValue()}`);
+    formData.append('hidden', `${challenge.getHidden()}`);
+
+    const response = await fetch(`${Constants?.expoConfig?.extra?.apiUrl}/challenge`, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${await getData('auth_token')}`,
+      },
+    });
+
+    if (response?.status !== 200) return null;
+    return await response.json();
+  } catch (err) {
+    return null;
+  }
+};
+
+export const deleteChallenge = async (id: any) => {
+  const response = await fetch(`${Constants?.expoConfig?.extra?.apiUrl}/nft/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      Authorization: `Bearer ${await getData('auth_token')}`,
+    },
+  });
+
+  if (response?.status !== 200) return false;
+
+  return true;
+};
